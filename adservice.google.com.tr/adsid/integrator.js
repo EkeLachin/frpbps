b0491e3c0d13278c618601438eee1662fcf9e327
EkeLachin <104994662+EkeLachin@users.noreply.github.com>
2023-02-23 14:36:06 +0300

    Add files via upload

---
+++ adservice.google.com.tr/adsid/integrator.js
@@ -0,0 +1,7 @@
+processGoogleToken({
+    "newToken": "ChAIgLrcnwYQlN2p-czrj9cnEjkAAXqgYZDYgLtfCtU9HyXaAHIlgXvVUQk--UwJRSF_f7yobxUfiTPVgQzuotp4_9wLX4U4yW1vb_Q",
+    "validLifetimeSecs": 300,
+    "freshLifetimeSecs": 300,
+    "1p_jar": "",
+    "pucrd": ""
+});
---
+++ adservice.google.com/adsid/integrator.js
@@ -0,0 +1,7 @@
+processGoogleToken({
+    "newToken": "ChAIgLrcnwYQlN2p-czrj9cnEjkAAXqgYbW4nHIdDqZjcyuxWoeKSsIJ-7BCtyDdUub1L0Mfyo7fd_53qb0TVbUQzNalad0_8Y2OR3s",
+    "validLifetimeSecs": 300,
+    "freshLifetimeSecs": 300,
+    "1p_jar": "2023-02-23-11",
+    "pucrd": ""
+});
